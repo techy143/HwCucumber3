361ce3eaa5bfb19756ee93ad5a3eb1fd0eac814e
Tauhid Chowdhury <tuhidzmac@Tauhids-MacBook-Pro.local>
2020-05-30 16:44:42 -0400

    added new project

---
+++ src/test/java/stepdefination/StepDeps.java
@@ -0,0 +1,39 @@
+package stepdefination;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDeps {
+
+    @Given("I navigate to www.ebay.com")
+    public void i_navigate_to_www_ebay_com() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Given("I click on login button")
+    public void i_click_on_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("I enter UserID and Password")
+    public void i_enter_UserID_and_Password() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Then("I should go to home page")
+    public void i_should_go_to_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @When("I enter wrong UserID and Password")
+    public void i_enter_wrong_UserID_and_Password() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+    @Then("I should see the error message")
+    public void i_should_see_the_error_message() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+}
